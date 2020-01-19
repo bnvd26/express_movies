58b13abf62c9ac005b91b6b7a545c7a3cf234f4f
Benjamin <benjamin.adida@hetic.net>
2020-01-19 14:44:45 +0100

    init EJS template

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 
 const PORT = 3000;
 
+// VIEWS
+
+app.set('views/', './views');
+app.set('view engine', 'ejs');
+
+
+// ROUTER
+
 app.get('/', (req, res) => {
   res.send('hello world !')
 })
 
 app.get('/movies', (req, res) => {
   res.send('movies')
+})
+
+app.get('/movies/add', (req, res) => {
+  res.send('formulaire d\'ajout')
 })
 
 app.get('/movies/:id', (req, res) => {
