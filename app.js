54e6d312fa9fa432961f5620034f2a8050e7d5ad
Benjamin <benjamin.adida@hetic.net>
2020-01-19 18:55:47 +0100

    tempalte EJS

--- app.js
+++ app.js
@@ -9,25 +9,33 @@ app.set('views/', './views');
 app.set('view engine', 'ejs');
 
 
+// MIDDLEWARE
+
+// use permet d'indiquer quelles sont les middleware à utiliser
+// ici ici on ajoute le middleware afin de pouvoir utiliser des fichiers statics
+
+app.use('/public',express.static('public'));
+
+
 // ROUTER
 
 app.get('/', (req, res) => {
-  res.send('hello world !')
+  res.render('index');
 })
 
 app.get('/movies', (req, res) => {
-  res.send('movies')
+  res.render('movies/index');
 })
 
 app.get('/movies/add', (req, res) => {
-  res.send('formulaire d\'ajout')
+  res.send('formulaire d\'ajout');
 })
 
 app.get('/movies/:id', (req, res) => {
   const id = req.params.id;
-  res.send(`film number ${id}`)
+  res.render('movies/show', { movieId : id });
 })
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${3000}`)
+  console.log(`Server is listening on port ${3000}`);
 })
